48df011b4d8235e2f146f54099cde4d791773beb
Steven Barrett <damentz@gmail.com>
2010-10-04 20:12:56 -0500

    sched-bfs: schedule groups as entity
    Group tasks by their group leader and distribute their CPU as though
    they're one task.
    
    The practical upshot of this is that each application is treated as one task
    no matter how many threads it starts or children it forks.
    
    The significance of this is that massively multithreaded applications such as
    java applications do not get any more cpu than if they were not threaded.
    
    The unexpected side effect is that doing make -j (any number) will, provided
    you don't run out of ram, feel like no more load than make -j1 no matter how
    many CPUs you have.
    
    Note that this drastically changes the way CPU is proportioned under load, as
    each application is seen as only one entity regardless of how many children
    it forks or threads. 'nice' is still respected.
    
    For example, on my quad core machine, running make -j128 feels like no more
    load than make -j1 except for when disk I/O occurs. The make -j128 proceeds
    at a rate ever so slightly slower than the make -j4 (which is still optimal).
    
    This will need extensive testing to see what disadvantages may occur, as some
    applications may have depended on getting more CPU by running multiple
    processes. So far I have yet to encounter a workload where this is a problem.
    Note that firefox, for example, has many threads and is contained as one
    application with this patch.
    
    It requires a change in mindset about how CPU is distributed in different
    workloads but I believe will be ideal for the desktop user. Think of it as
    implementing everything you want out of the complex CGROUPS for grouping tasks
    as an entity, but without the overhead or any input or effort on the user's
    part.
    
    -ck

--- kernel/sched_bfs.c
+++ kernel/sched_bfs.c
@@ -672,11 +672,26 @@ static int isoprio_suitable(void)
 	return !grq.iso_refractory;
 }
 
+static inline u64 task_deadline_diff(struct task_struct *p);
+
 /*
  * Adding to the global runqueue. Enter with grq locked.
  */
 static void enqueue_task(struct task_struct *p)
 {
+	s64 max_tdd;
+
+	max_tdd = task_deadline_diff(p);
+
+	/*
+	 * Make sure that when we're queueing this task again that it
+	 * doesn't have any old deadliens from when the program group was
+	 * being penalised and adjust the deadline to the highest it should
+	 * be, based on the current number of threads running.
+	 */
+	max_tdd *= p->group_leader->threads_running;
+	if (p->deadline - grq.niffies > max_tdd)
+		p->deadline = grq.niffies + max_tdd;
 	if (!rt_task(p)) {
 		/* Check it hasn't gotten rt from PI */
 		if ((idleprio_task(p) && idleprio_suitable(p)) ||
@@ -976,10 +991,13 @@ static int effective_prio(struct task_struct *p)
 }
 
 /*
- * activate_task - move a task to the runqueue. Enter with grq locked.
+ * activate_task - move a task to the runqueue. Enter with grq locked. The
+ * number of threads running is stored in the group_leader struct.
  */
 static void activate_task(struct task_struct *p, struct rq *rq)
 {
+	unsigned long *threads_running = &p->group_leader->threads_running;
+
 	update_clocks(rq);
 
 	/*
@@ -996,6 +1014,13 @@ static void activate_task(struct task_struct *p, struct rq *rq)
 	p->prio = effective_prio(p);
 	if (task_contributes_to_load(p))
 		grq.nr_uninterruptible--;
+	/*
+	 * Adjust deadline according to number of running tasks/threads within
+	 * this program group. This ends up distributing CPU to the program
+	 * group as a single entity.
+	 */
+	if (++*threads_running > 1)
+		p->deadline += task_deadline_diff(p);
 	enqueue_task(p);
 	grq.nr_running++;
 	inc_qnr();
@@ -1007,9 +1032,13 @@ static void activate_task(struct task_struct *p, struct rq *rq)
  */
 static inline void deactivate_task(struct task_struct *p)
 {
+	unsigned long *threads_running = &p->group_leader->threads_running;
+
 	if (task_contributes_to_load(p))
 		grq.nr_uninterruptible++;
 	grq.nr_running--;
+	if (--*threads_running > 0)
+		p->deadline -= task_deadline_diff(p);
 }
 
 #ifdef CONFIG_SMP
@@ -2508,8 +2537,20 @@ static inline int ms_longest_deadline_diff(void)
  */
 static void time_slice_expired(struct task_struct *p)
 {
+	unsigned long *threads_running = &p->group_leader->threads_running;
+	u64 tdd = task_deadline_diff(p);
+
+	/*
+	 * We proportionately increase the deadline according to how many
+	 * threads are running. This effectively makes a thread group have
+	 * the same CPU as one task, no matter how many threads are running.
+	 * time_slice_expired can be called when there may be none running
+	 * when p is deactivated so we must explicitly test for more than 1.
+	 */
+	if (*threads_running > 1)
+		tdd *= *threads_running;
 	p->time_slice = timeslice();
-	p->deadline = grq.niffies + task_deadline_diff(p);
+	p->deadline = grq.niffies + tdd;
 }
 
 /*
@@ -3463,7 +3504,7 @@ SYSCALL_DEFINE1(nice, int, increment)
  *
  * This is the priority value as seen by users in /proc.
  * RT tasks are offset by -100. Normal tasks are centered around 1, value goes
- * from 0 (SCHED_ISO) up to 82 (nice +19 SCHED_IDLEPRIO).
+ * from 0 (SCHED_ISO) up to ~900 (nice +19 SCHED_IDLEPRIO).
  */
 int task_prio(const struct task_struct *p)
 {
@@ -3476,7 +3517,7 @@ int task_prio(const struct task_struct *p)
 	/* Convert to ms to avoid overflows */
 	delta = NS_TO_MS(p->deadline - grq.niffies);
 	delta = delta * 40 / ms_longest_deadline_diff();
-	if (delta > 0 && delta <= 80)
+	if (delta > 0)
 		prio += delta;
 	if (idleprio_task(p))
 		prio += 40;
