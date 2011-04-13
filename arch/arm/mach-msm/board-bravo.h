be84b22f30fb14dc6ed303d55b93f50078d1565c
vork <boss.benedikt@googlemail.com>
2011-04-13 22:53:21 +0200

    Tidy up, update defconfigs, advanced config...
    No voltage choice anymore. I have implemented snq's vdd level sysfs interface.
    No HAVS support anymore. If you know how you can enable it, but it isn't removed from the kernel (need to be enabled in config).
    Smartass govenore (tweaked ofcourse ;)).
    Dropped BFS support. Same as HAVS. If you know how it is still buildable.
    All in all slimmed down build script + new kernel naming support.
    And a small welcome in recovery. Yep very small :D

--- arch/arm/mach-msm/board-bravo.h
+++ arch/arm/mach-msm/board-bravo.h
@@ -182,4 +182,7 @@
 
 #define is_cdma_version(rev) (rev == 0x05)
 
+#define BRAVO_TPS65023_MIN_UV_MV    (800)
+#define BRAVO_TPS65023_MAX_UV_MV    (1350)
+
 #endif /* __ARCH_ARM_MACH_MSM_BOARD_BRAVO_H */
--- compile.sh
+++ compile.sh
@@ -11,31 +11,37 @@ make ARCH=arm CROSS_COMPILE=$ARM_EABI INSTALL_MOD_PATH=$DIR/Tools/AnyKernel/syst
 # signed_file variables
 
 # build date
-NOW=$(date +"%d-%b-%y")
-# linux version + localversion
-localVersion=`cat .config | fgrep CONFIG_LOCALVERSION= | cut -f 2 -d= | sed s/\"//g`
-linuxVersion=`cat .config | fgrep "Linux/arm " | cut -d: -f 1 | cut -c13-20`
-VERSION=$linuxVersion$localVersion
+# NOW=$(date +"%d-%b-%y")
+ linux version + localversion
+ localVersion=`cat .config | fgrep CONFIG_LOCALVERSION= | cut -f 2 -d= | sed s/\"//g`
+ linuxVersion=`cat .config | fgrep "Linux/arm " | cut -d: -f 1 | cut -c13-20`
+ VERSION=$linuxVersion$localVersion
+
+if grep -q TESTVR arch/arm/configs/vorkKernelSVS_bravo_defconfig
+	$subdir=TEST
+else
+	$subdir=download
+fi
 
 # CPU/SLAB/IO
-CpuSched=`cat .config | grep CONFIG_SCHED_.FS=y | cut -c14-16`
-Slab=`cat .config | grep CONFIG_SL.B=y | cut -c8-11`
-tmp=`cat .config | fgrep CONFIG_DEFAULT_IOSCHED= | cut -f 2 -d= | sed s/\"//g`
-IOSched=$( echo "$tmp" | tr -s  '[:lower:]'  '[:upper:]' )
+# CpuSched=`cat .config | grep CONFIG_SCHED_.FS=y | cut -c14-16`
+# Slab=`cat .config | grep CONFIG_SL.B=y | cut -c8-11`
+# tmp=`cat .config | fgrep CONFIG_DEFAULT_IOSCHED= | cut -f 2 -d= | sed s/\"//g`
+# IOSched=$( echo "$tmp" | tr -s  '[:lower:]'  '[:upper:]' )
 
 # HAVS or SVS
-AVS=`cat .config | grep CONFIG_MSM_CPU_AVS=y | cut -c20-21`
-if [ "$AVS" == "y" ]
-	then
-			voltage=`cat .config | grep CONFIG_AVS_...=y | cut -c12-14`
-			SVSHAVS=HAVS-$voltage
-		else
-			voltage=`cat .config | grep CONFIG_SVS_...=y | cut -c12-14`
-			SVSHAVS=SVS-$voltage
-	fi
+# AVS=`cat .config | grep CONFIG_MSM_CPU_AVS=y | cut -c20-21`
+# if [ "$AVS" == "y" ]
+#	then
+#			voltage=`cat .config | grep CONFIG_AVS_...=y | cut -c12-14`
+#			SVSHAVS=HAVS-$voltage
+#		else
+#			voltage=`cat .config | grep CONFIG_SVS_...=y | cut -c12-14`
+#			SVSHAVS=SVS-$voltage
+#	fi
 
 
-signed_file=vorkKernel-$linuxVersion$GC$CAM-$SVSHAVS-$CpuSched-$IOSched-$Slab-$NOW.zip
+# signed_file=vorkKernel-$linuxVersion$GC$CAM-$SVSHAVS-$CpuSched-$IOSched-$Slab-$NOW.zip
 
 cp arch/arm/boot/zImage Tools/AnyKernel/kernel
 rm Tools/AnyKernel/system/lib/modules/$VERSION/build
@@ -60,10 +66,10 @@ rm -f testkey.*
 rm -f signapk.jar
 rm -f update.zip
 
-if [ -d $BUILD_DIR/$linuxVersion ]; then
-mv $signed_file $BUILD_DIR/$linuxVersion/$signed_file
+if [ -d $BUILD_DIR/$subdir ]; then
+mv $signed_file $BUILD_DIR/$subdir/$signed_file
 else
-    mkdir $BUILD_DIR/$linuxVersion
-    mv $signed_file $BUILD_DIR/$linuxVersion/$signed_file
+    mkdir $BUILD_DIR/$subdir
+    mv $signed_file $BUILD_DIR/$subdir/$signed_file
 fi
 cd ../../
